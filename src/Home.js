b36f2c01a85d3f697d630388c5ec3744278303ea
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-05-07 20:07:28 -0300

    Add files via upload

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,72 @@ import { Link } from 'react-router-dom';
 import './index.css';
 const Home = () =>{
     return (
-        <div className='Index'>
+        <div className="Index">
+        <header className="Index-header">
+            <nav className="navbar">
+                <div>
+                    <h1>Atividades React - Fatec Presidente Prudente</h1>
+                </div>
+            </nav>
+            <ul>
+                <li>
+                    <Link to="/Atv01"><button className="btn-voltar">Atividade 1</button></Link>
+                </li>
+                <li>
+                    <Link to="/Atv02"><button className="btn-voltar">Atividade 2</button></Link>
+                </li>
+            </ul>
+        </header>
+</div>            
+    );
+}
+export default Home;
+/** feio mas funciona
+ *       <div>
+            <h1>Atividades React - Fatec Presidente Prudente</h1>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to="/Atv01">Atividade 01</Link>
+                    </li>
+                    <li>
+                        <Link to="/Atv02">Atividade 02</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+ */
+
+/** bonito mas n funciona
+ * <div className="Index">
+            <header className="Index-header">
+                <nav className="navbar">
+                    <div>
+                        <h1>Atividades React - Fatec Presidente Prudente</h1>
+                    </div>
+                </nav>
+                <ul>
+                    <li>
+                        <Link to="/Atv01"><button className="btn-voltar">Atividade 1</button></Link>
+                    </li>
+                    <li>
+                        <Link to="/Atv02>"><button className="btn-voltar">Atividade 2</button></Link>
+                    </li>
+                    
+                </ul>
+            </header>
+    </div>  
+ */
+
+
+
+
+
+
+
+
+/** github e funciona
+ *         <div className='Index'>
             <header className='Index-header'>
                     <nav className='navbar'>
                         <div>
@@ -19,23 +84,5 @@ const Home = () =>{
                             </li>
                         </ul>
             </header>
-        </div>
-        
-    );
-}
-export default Home;
-/**
- *       <div>
-            <h1>Atividades React - Fatec Presidente Prudente</h1>
-            <nav>
-                <ul>
-                    <li>
-                        <Link to="/Atv01">Atividade 01</Link>
-                    </li>
-                    <li>
-                        <Link to="/Atv02">Atividade 02</Link>
-                    </li>
-                </ul>
-            </nav>
         </div>
  */
